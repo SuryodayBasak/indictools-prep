1cc3791fbc70026abb7d9bf86d533a783848ffaf
Suryoday Basak <suryodaybasak@gmail.com>
2015-05-29 01:50:43 +0530

    Introducing statistical analysis to select words for annotation based on word height

--- src/wordseg.py
+++ src/wordseg.py
@@ -8,32 +8,50 @@ Created on Wed May 27 22:52:39 2015
 import cv2
 import prep2
 import numpy as np
-kernel = np.ones((2,4),np.uint8)
+import math
+kernel1 = np.ones((2,3),np.uint8)
+kernel2 = np.ones((1,1),np.uint8)
 
-
-img = cv2.imread('/home/suryo/Image_Processing_Exercises/indictools-prep/resources/durga.jpg',0)
+all_heights = [] 
+img = cv2.imread('/home/suryo/Image_Processing_Exercises/indictools-prep/resources/Kandanu1.jpg',0)
 cv2.imshow('Output0',img)
 words_temp = np.zeros(img.shape[:2],np.uint8)
 
 binary = prep2.binary_img(img)
 
-dilation = cv2.dilate(binary,kernel,iterations = 1)
-cv2.imshow('d',dilation)
+dilation = cv2.dilate(binary,kernel1,iterations = 1)
+erosion = cv2.dilate(dilation,kernel1,iterations = 1)
 
-contours, hierarchy = cv2.findContours(dilation,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.imshow('d',erosion)
+
+contours, hierarchy = cv2.findContours(erosion,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 for xx in contours:
     cv2.drawContours(words_temp,[xx],-1,(255,255,255),-1)
 cv2.imshow('Outputtemp0',words_temp)
     
+
 contours, hierarchy = cv2.findContours(words_temp,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 for c in contours:
     x,y,w,h = cv2.boundingRect(c)
     #img = 
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0),2)
+    #cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0),2)
+    all_heights.append(h)
+
+print all_heights
+mean_height = np.mean(all_heights)
+print mean_height
+std_dev = np.std(all_heights)
+print std_dev
+
+for c in contours:
+    x,y,w,h = cv2.boundingRect(c)
+    if(math.fabs(h-mean_height)<=(std_dev)):
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0),2)
+
 
 cv2.imshow('Outputtemp',img)
-cv2.imwrite('wordseg3.jpg',img)
+#cv2.imwrite('wordseg3.jpg',img)
 
             
             
