7d0ee087d1ed54999ae6a88e5f80f69e48d74df1
Suryoday Basak <suryodaybasak@gmail.com>
2015-06-11 17:03:06 +0530

    Trying sobel-y for character segmentation

---
+++ src/charseg1.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 16:36:47 2015
+
+@author: suryo
+"""
+
+import cv2
+import prep2
+import numpy as np
+kernel1 = np.ones((1,1),np.uint8)
+kernel2 = np.ones((1,1),np.uint8)
+
+all_heights = [] 
+img = cv2.imread('/home/suryo/Image_Processing_Exercises/indictools-prep/resources/Kandanu1.jpg',0)
+cv2.imshow('Output0',img)
+words_temp = np.zeros(img.shape[:2],np.uint8)
+
+binary = prep2.binary_img(img)
+cv2.imshow('binary',binary)
+
+sobelx = cv2.Sobel(binary,cv2.CV_64F,1,0,ksize=5)
+cv2.imshow('sobel',sobelx)
+
+sobely = cv2.Sobel(binary,cv2.CV_64F,1,0,ksize=5)
+cv2.imshow('sobely',sobely)
+
+abs_sobel64f = np.absolute(sobelx)
+sobel_8u = np.uint8(abs_sobel64f)
+
+cv2.imshow('8u',sobel_8u)
+
+            
+dilate_sobel = cv2.dilate(sobel_8u,kernel1,iterations = 1)
+cv2.imshow('dilate sobel',dilate_sobel)
+
+opening = cv2.morphologyEx(dilate_sobel, cv2.MORPH_OPEN, kernel1)
+cv2.imshow('open sobel',opening)
+
+dilation = cv2.dilate(binary,kernel1,iterations = 1)
+erosion = cv2.dilate(dilation,kernel1,iterations = 1)
+
+cv2.imshow('d',erosion)
+edges = cv2.Canny(erosion,100,200)
+cv2.imshow('canny',edges)
+
+"""
+contours, hierarchy = cv2.findContours(erosion,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+for xx in contours:
+    cv2.drawContours(words_temp,[xx],-1,(255,255,255),-1)
+cv2.imshow('Outputtemp0',words_temp)
+    
+contours, hierarchy = cv2.findContours(words_temp,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+for c in contours:
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0),2)
+
+cv2.imshow('Outputimg2',img)
+"""
+
+contours, hierarchy = cv2.findContours(sobel_8u,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+for xx in contours:
+    cv2.drawContours(words_temp,[xx],-1,(255,255,255),-1)
+cv2.imshow('Outputtemp0',words_temp)
+    
+contours, hierarchy = cv2.findContours(words_temp,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+for c in contours:
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0),2)
+
+cv2.imshow('Outputimg2',img)
+          
+cv2.waitKey(0)
+cv2.destroyAllWindows()
